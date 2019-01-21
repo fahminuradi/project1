0139a4eddbead295badb7bc7685e88b650be5cdc
RPebriansyah02 <rezapebriansyah2002@gmail.com>
2019-01-21 11:01:50 +0700

    jadwal

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 use App\Jadwal;
+use App\Profile;
 
 class JadwalController extends Controller
 {
@@ -15,7 +17,7 @@ class JadwalController extends Controller
     public function index()
     {
         $jadwals = Jadwal::latest()->paginate(5);
-        return view('jadwal.index', compact('jadwals'))
+        return view('profile.index', compact('jadwals'))
                 ->with('i', (request()->input('page',1) -1)*5);
     }
 
@@ -37,7 +39,7 @@ class JadwalController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+            $request->validate([
             'mapel1' => 'required',
             'mapel2' => 'required',
             'mapel3' => 'required',
@@ -49,6 +51,28 @@ class JadwalController extends Controller
             'guru4' => 'required',
             'guru5' => 'required'
         ]);
+        // $data = new \App\Jadwal();
+        $profiles = DB::table('profiles')->max('id');
+        //Grade::create($request->all());
+        $file= new Jadwal();
+        $file->profile_id=$profiles;
+        $file->mapel1 = $request->input('mapel1');
+        $file->mapel2 = $request->input('mapel2');
+        $file->mapel3 = $request->input('mapel3');
+        $file->mapel4 = $request->input('mapel4');
+        $file->mapel5 = $request->input('mapel5');
+        $file->guru1 = $request->input('guru1');
+        $file->guru2 = $request->input('guru2');
+        $file->guru3 = $request->input('guru3');
+        $file->guru4 = $request->input('guru4');
+        $file->guru5 = $request->input('guru5');
+        //$path = $request->file('avatar')->store('avatars');
+        //$data->avatar=$path;
+        //$fileName = rand(300,300).'.'.$ext;
+        //$file->move('uploads/avatars',$fileName);
+        $file->save();
+        return redirect()->route('profile.index')
+                        ->with('success', 'jadwal berhasil ditambahkan');
         $data = new \App\Jadwal();
         $data->mapel1 = $request->input('mapel1');
         $data->mapel2 = $request->input('mapel2');
@@ -72,8 +96,18 @@ class JadwalController extends Controller
      */
     public function show($id)
     {
-        $jadwal = Jadwal::find($id);
-        return view('jadwal.detail', compact('jadwal'));
+        $file->id_profile=$profiles;
+        $file->mapel1 = $request->get('mapel1');
+        $file->mapel2 = $request->get('mapel2');
+        $file->mapel3 = $request->get('mapel3');
+        $file->mapel4 = $request->get('mapel4');
+        $file->mapel5 = $request->get('mapel5');
+        $file->guru1 = $request->get('guru1');
+        $file->guru2 = $request->get('guru2');
+        $file->guru3 = $request->get('guru3');
+        $file->guru4 = $request->get('guru4');
+        $file->guru5 = $request->get('guru5');
+        return view('profile.detail', compact('jadwals'));
     }
 
     /**
@@ -84,8 +118,8 @@ class JadwalController extends Controller
      */
     public function edit($id)
     {
-        $jadwal = Jadwal::find($id);
-        return view('jadwal.edit', compact('jadwal'));
+        $profile = Profile::find($id);
+        return view('profile.edit', compact('profile'));
     }
 
     /**
@@ -97,32 +131,32 @@ class JadwalController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $request->validate([
-            'mapel1' => 'required',
-            'mapel2' => 'required',
-            'mapel3' => 'required',
-            'mapel4' => 'required',
-            'mapel5' => 'required',
-            'guru1' => 'required',
-            'guru2' => 'required',
-            'guru3' => 'required',
-            'guru4' => 'required',
-            'guru5' => 'required'
-        ]);
-        $jadwal = Jadwal::find($id);
-        $jadwal->mapel1 = $request->input('mapel1');
-        $jadwal->mapel2 = $request->input('mapel2');
-        $jadwal->mapel3 = $request->input('mapel3');
-        $jadwal->mapel4 = $request->input('mapel4');
-        $jadwal->mapel5 = $request->input('mapel5');
-        $jadwal->guru1 = $request->input('guru1');
-        $jadwal->guru2 = $request->input('guru2');
-        $jadwal->guru3 = $request->input('guru3');
-        $jadwal->guru4 = $request->input('guru4');
-        $jadwal->guru5 = $request->input('guru5');
-        $jadwal->save();
-        return redirect()->route('jadwal.index')
-                        ->with('success', 'Biodata murid berhasil diubah');
+        // $request->validate([
+        //     'mapel1' => 'required',
+        //     'mapel2' => 'required',
+        //     'mapel3' => 'required',
+        //     'mapel4' => 'required',
+        //     'mapel5' => 'required',
+        //     'guru1' => 'required',
+        //     'guru2' => 'required',
+        //     'guru3' => 'required',
+        //     'guru4' => 'required',
+        //     'guru5' => 'required'
+        // ]);
+        // $jadwal = Jadwal::find($id);
+        // $jadwal->mapel1 = $request->input('mapel1');
+        // $jadwal->mapel2 = $request->input('mapel2');
+        // $jadwal->mapel3 = $request->input('mapel3');
+        // $jadwal->mapel4 = $request->input('mapel4');
+        // $jadwal->mapel5 = $request->input('mapel5');
+        // $jadwal->guru1 = $request->input('guru1');
+        // $jadwal->guru2 = $request->input('guru2');
+        // $jadwal->guru3 = $request->input('guru3');
+        // $jadwal->guru4 = $request->input('guru4');
+        // $jadwal->guru5 = $request->input('guru5');
+        // $jadwal->save();
+        // return redirect()->route('jadwal.index')
+        //                 ->with('success', 'Biodata murid berhasil diubah');
     }
 
     /**
@@ -133,9 +167,9 @@ class JadwalController extends Controller
      */
     public function destroy($id)
     {
-        $jadwal = Jadwal::find($id);
-        $jadwal->delete();
-        return redirect()->route('jadwal.index')
-                        ->with('success', 'Biodata murid berhasil dihapus');
+        // $jadwal = Jadwal::find($id);
+        // $jadwal->delete();
+        // return redirect()->route('jadwal.index')
+        //                 ->with('success', 'Biodata murid berhasil dihapus');
     }
 }
